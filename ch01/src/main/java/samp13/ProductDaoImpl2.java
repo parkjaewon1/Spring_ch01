3924f9e8fbc9412ebb0588a513f663c0a7c2c513
parkjaewon1 <qkrwodnjs616@naver.com>
2021-06-10 11:33:37 +0900

    2021-06-10

--- ch01/src/main/java/samp13/ProductDaoImpl2.java
+++ ch01/src/main/java/samp13/ProductDaoImpl2.java
@@ -3,9 +3,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 // @붙어 있는 것이 annotaion
 // @Component  // 객체(componet)로 만들어 줘라
-@Repository("pd1")
-public class ProductDaoImpl implements ProductDao {
+@Repository("pd2")
+public class ProductDaoImpl2 implements ProductDao {
 	public Product getProduct(String name) {
-		return new Product(name, 2000);
+		return new Product(name, 5000);
 	}
 }
--- ch01/src/main/java/samp13/ProductServiceImpl.java
+++ ch01/src/main/java/samp13/ProductServiceImpl.java
@@ -1,9 +1,13 @@
 package samp13;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-@Component
+import org.springframework.stereotype.Service;
+// @Component
+@Service
 public class ProductServiceImpl implements ProductService{
 	@Autowired // setter method가 필요 없음 // 생성된 객체와 연결
+	@Qualifier("pd2")
 	private ProductDao pd;
 	public Product getProduct() {
 		return pd.getProduct("볼펜");
